1da7f35ae55fdc926956a15964970d26c939c259
JuliaSyz <31566110+JuliaSyz@users.noreply.github.com>
2021-01-27 19:25:45 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Pentagram from './Pentagram.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={Pentagram} className="App-logo" alt="Pentagram" />
+        <p>
+          Hello
+        </p>
+        <a
+          
+        >
+          
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
